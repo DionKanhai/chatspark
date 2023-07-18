14bfaa31f009a0d2e4ba9e8163218ada7f83b40a
Dion Kanhai <dionkanhai@Dions-MacBook-Air.local>
2023-07-17 20:22:21 -0400

    edit server file

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-//imports the express framework into the server file
 const express = require('express');
-//creartes an instance of the express application
 const app = express();
-// PORT variable defines the port on which the server will listen
-// it is set to the env variable PORT, otherwise default 3000
 const PORT = process.env.PORT || 3000;
 
 // define your routes and middleware here
