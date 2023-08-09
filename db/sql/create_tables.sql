1b5811ecdcc126ff2e14b87afa2043c61c4ee2d6
Dion Kanhai <dionkanhai@dions-air.home>
2023-08-09 13:39:33 -0400

    edit table for db setup

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -1,7 +1,11 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS user_friends CASCADE;
+DROP TABLE IF EXISTS user_friend_requests CASCADE;
 DROP TABLE IF EXISTS user_blocked CASCADE;
+DROP TABLE IF EXISTS user_sessions CASCADE;
 DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS user_chat_groups CASCADE;
+DROP TABLE IF EXISTS user_chat_group_members CASCADE;
 
 
 CREATE TABLE users (
@@ -14,6 +18,14 @@ CREATE TABLE users (
 CREATE TABLE user_friends (
   id SERIAL PRIMARY KEY NOT NULL,
   users_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE user_friend_requests (
+  id SERIAL PRIMARY KEY,
+  sender_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  receiver_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  request_status VARCHAR(10) NOT NULL,
+  UNIQUE (sender_id, receiver_id)
 );
 
 CREATE TABLE user_blocked (
@@ -29,3 +41,24 @@ CREATE TABLE messages (
   timestamp TIMESTAMPTZ DEFAULT NOW(),
   message_text TEXT NOT NULL,
 );
+
+CREATE TABLE user_sessions (
+  id PRIMARY SERIAL KEY NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  session_token VARCHAR(100) NOT NULL,
+  expiration_date TIMESTAMP NOT NULL,
+  UNIQUE (user_id, session_token)
+);
+
+CREATE TABLE user_chat_groups (
+  id SERIAL PRIMARY KEY NOT NULL,
+  group_name VARCHAR(100) NOT NULL,
+  group_description TEXT,
+  created_by INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_chat_group_members (
+  id SERIAL PRIMARY KEY NOT NULL,
+  group_id INTEGER NOT NULL REFERENCES user_chat_groups(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE 
+)
